c55e974e8703e96975b4ed6e78a0eef1230a5538
amolankit <amolankit@live.com>
2024-08-12 22:33:15 +0100

    Initial commit with DI scopes

---
+++ DependencyInjectionSample/InterfaceForDi.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInjectionSample
+{
+    public interface IGuidSingleton : IGuidGenerator { }
+    public interface IGuidScoped : IGuidGenerator { }
+    public interface IGuidTransient : IGuidGenerator { }
+
+    public interface IGuidGenerator
+    {
+        Guid Value { get; }
+    }
+
+    public class GuidGenerator : IGuidSingleton, IGuidScoped, IGuidTransient
+    {
+        public Guid Value { get; private set; } = Guid.NewGuid();
+    }
+}
+
